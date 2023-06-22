d6e140291edb37226c8345d5ca9548d142ba52b9
Michael <mcarroll138@gmail.com>
2023-06-22 13:19:12 -0700

    pass test one Haiku constructor.

--- __tests__/haiku-creator.test.js
+++ __tests__/haiku-creator.test.js
@@ -3,9 +3,9 @@ import Haiku from '../src/haiku-creator.js';
 describe('Haiku', () => {
     test('should create a haiku object with three lines', () => {
         const haiku = new Haiku("a", "b", "c");
-        expect(haiku.line1).toMatch();
-        expect(haiku.line2).toMatch();
-        expect(haiku.line3).toMatch();
+        expect(haiku.line1).toMatch("a");
+        expect(haiku.line2).toMatch("b");
+        expect(haiku.line3).toMatch("c");
     })
 
 });
